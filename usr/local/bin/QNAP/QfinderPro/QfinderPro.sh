b45df7c9855c529f3d5d042f017f78578f6e6396
EvilSupahFly <seann.giffin@gmail.com>
2025-07-07 18:12:24 -0400

    Update QfinderPro.sh

--- usr/local/bin/QNAP/QfinderPro/QfinderPro.sh
+++ usr/local/bin/QNAP/QfinderPro/QfinderPro.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
-appname=`basename $0 | sed s,\.sh$,,`
+appname=$(basename "$0" | sed 's,\.sh$,,')
+dirname=$(cd "$(dirname "$0")" && pwd)
 
-dirname=`dirname $0`
-tmp="${dirname#?}"
+export LD_LIBRARY_PATH="$dirname"
+export QT_PLUGIN_PATH="$dirname/plugins"
 
-if [ "${dirname%$tmp}" != "/" ]; then
-dirname=$PWD/$dirname
-fi
-LD_LIBRARY_PATH="$dirname/lib"
-export LD_LIBRARY_PATH=/usr/local/lib/QNAP/QfinderPro
-$dirname/$appname "$@"
+exec "$dirname/$appname" "$@"
