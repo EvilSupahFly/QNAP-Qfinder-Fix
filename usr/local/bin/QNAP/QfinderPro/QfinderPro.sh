41c2d2422451ba89af6c58910077b3c7b7e87010
EvilSupahFly <seann.giffin@gmail.com>
2025-07-07 20:27:27 -0400

    Update QfinderPro.sh

--- usr/local/bin/QNAP/QfinderPro/QfinderPro.sh
+++ usr/local/bin/QNAP/QfinderPro/QfinderPro.sh
@@ -2,9 +2,9 @@
 appname=$(basename "$0" | sed 's,\.sh$,,')
 dirname=$(cd "$(dirname "$0")" && pwd)
 
-export LD_LIBRARY_PATH="$dirname"
-export QT_PLUGIN_PATH="$dirname/plugins"
-export QT_QPA_PLATFORM_PLUGIN_PATH="$dirname/plugins/platforms"
+export LD_LIBRARY_PATH="/usr/local/lib/QNAP/QfinderPro"
+export QT_PLUGIN_PATH="$dirname"
+export QT_QPA_PLATFORM_PLUGIN_PATH="$dirname/platforms"
 export QT_DEBUG_PLUGINS=1
 
 exec "$dirname/$appname" "$@"
